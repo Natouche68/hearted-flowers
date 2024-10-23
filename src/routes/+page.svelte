48626021f9f8111f934a8cb76ae1656c178656dc
Natouche68 <ngrossheny@gmail.com>
2024-10-23 12:40:00 +0200

    Create a container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,4 @@
-<h1>Hearted Flowers</h1>
+<div class="h-dvh flex justify-center items-center bg-[#272932]">
+	<svg viewBox="0 0 100 100" class="m-6 object-contain max-w-xl bg-[#f49097]">
+	</svg>
+</div>
