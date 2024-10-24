e9c6a7499350a1a6dacc338ede6aa9de225a98f5
Natouche68 <ngrossheny@gmail.com>
2024-10-24 17:15:33 +0200

    Add rotation animations

--- src/lib/Flower.svelte
+++ src/lib/Flower.svelte
@@ -20,44 +20,67 @@
 	);
 </script>
 
-<line
-	x1={flower?.base.x}
-	y1={flower?.base.y}
-	x2={flower?.top.x}
-	y2={flower?.top.y}
-	stroke="url(#stem)"
-	stroke-width="1.5"
-/>
+<g
+	transform-origin="{flower?.base.x ?? 0} {flower?.base.y ?? 0}"
+	class="flower"
+	style="--rotation-duration: {8 + Math.random() * 4}s;"
+>
+	<line
+		x1={flower?.base.x}
+		y1={flower?.base.y}
+		x2={flower?.top.x}
+		y2={flower?.top.y}
+		stroke="url(#stem)"
+		stroke-width="1.5"
+	/>
 
-{#each new Array(5) as _, i}
-	<Heart
-		x={flower?.top.x ?? 0}
-		y={flower?.top.y ?? 0}
-		scale={(flower?.petalScale ?? 0) + Math.random() * 5}
-		rotation={i * (360 / 5) + Math.random() * 20}
-		fill="petal"
-		originOffset={90}
-		transformOffset={90}
-	/>
-{/each}
+	{#each new Array(5) as _, i}
+		<Heart
+			x={flower?.top.x ?? 0}
+			y={flower?.top.y ?? 0}
+			scale={(flower?.petalScale ?? 0) + Math.random() * 5}
+			rotation={i * (360 / 5) + Math.random() * 20}
+			fill="petal"
+			originOffset={90}
+			transformOffset={90}
+		/>
+	{/each}
 
-{#each new Array(3) as _, i}
-	<Heart
-		x={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.cos(angle) +
-			(flower?.base.x ?? 0)}
-		y={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.sin(angle) +
-			(flower?.base.y ?? 0)}
-		scale={(flower?.leafScale ?? 0) + Math.random() * 3}
-		rotation={angle * (180 / Math.PI) + 45 + Math.random() * 20}
-		fill="leaf"
-	/>
-	<Heart
-		x={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.cos(angle) +
-			(flower?.base.x ?? 0)}
-		y={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.sin(angle) +
-			(flower?.base.y ?? 0)}
-		scale={(flower?.leafScale ?? 0) + Math.random() * 3}
-		rotation={angle * (180 / Math.PI) + 125 + Math.random() * 20}
-		fill="leaf"
-	/>
-{/each}
+	{#each new Array(3) as _, i}
+		<Heart
+			x={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.cos(angle) +
+				(flower?.base.x ?? 0)}
+			y={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.sin(angle) +
+				(flower?.base.y ?? 0)}
+			scale={(flower?.leafScale ?? 0) + Math.random() * 3}
+			rotation={angle * (180 / Math.PI) + 45 + Math.random() * 20}
+			fill="leaf"
+		/>
+		<Heart
+			x={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.cos(angle) +
+				(flower?.base.x ?? 0)}
+			y={(stemLength * 0.2 * (i + 1) + Math.random() * 5) * Math.sin(angle) +
+				(flower?.base.y ?? 0)}
+			scale={(flower?.leafScale ?? 0) + Math.random() * 3}
+			rotation={angle * (180 / Math.PI) + 125 + Math.random() * 20}
+			fill="leaf"
+		/>
+	{/each}
+</g>
+
+<style>
+	.flower {
+		animation: flower-rotating var(--rotation-duration) ease infinite;
+	}
+
+	@keyframes flower-rotating {
+		0%,
+		100% {
+			transform: rotate(-1deg);
+		}
+
+		50% {
+			transform: rotate(1deg);
+		}
+	}
+</style>
